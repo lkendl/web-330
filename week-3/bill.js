5140683377c39618f3832b4289552f6518f68689
Laura Kendl <lkendl@live.com>
2022-01-22 22:52:19 -0700

    omitted 'function' in functions

--- week-3/bill.js
+++ week-3/bill.js
@@ -10,54 +10,53 @@
 */
 // Bill class with properties: _beverages, _appetizers, _mainCourses, and _desserts.
 export class Bill {
-    constructor() {
-        this._beverages = [];
-        this._appetizers = [];
-        this._mainCourses = [];
-        this._desserts = [];
-    }
-}
+    _beverages = [];
+    _appetizers = [];
+    _mainCourses = [];
+    _desserts = [];
 
 // This function pushes the beverage to the _beverages array.
-function addBeverage(beverage) {
+addBeverage(beverage) {
     this._beverages.push(beverage);
 }
 
 // This function pushes the appetizer to the _appetizers array.
-function addAppetizer(appetizer) {
+addAppetizer(appetizer) {
     this._appetizers.push(appetizer);
 }
 
 // This function pushes the mainCourse to the _mainCourses array.
-function addMainCourse(mainCourse) {
+addMainCourse(mainCourse) {
     this._mainCourses.push(mainCourse);
 }
 
 // This function pushes the dessert to the _desserts array.
-function addDessert(dessert) {
+addDessert(dessert) {
     this._desserts.push(dessert);
 }
 
 // This function...
-function getTotal() {
+getTotal() {
+
     let total = 0;
 
-    let beverageTotal = this._beverages.forEach(function(beverage) {
+    this._beverages.forEach(function(beverage) {
         total += parseFloat(beverage.price);
     });
 
-    let appetizerTotal = this._appetizers.forEach(function(appetizer) {
+    this._appetizers.forEach(function(appetizer) {
         total += parseFloat(appetizer.price);
     });
 
-    let mainCourseTotal = this._mainCourses.forEach(function(mainCourse) {
+    this._mainCourses.forEach(function(mainCourse) {
         total += parseFloat(mainCourse.price);
     });
 
-    let dessertTotal = this._desserts.forEach(function(mainCourse) {
+    this._desserts.forEach(function(mainCourse) {
         total += parseFloat(dessert.price);
     });
 
     // Return the total variable and set to two decimal places.
     return total.toFixed(2);
+    }
 }
