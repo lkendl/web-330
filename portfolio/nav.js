a886027d0c97c4c0ee40b4f36f19ab2a15fce01e
Laura Kendl <lkendl@live.com>
2022-02-18 22:54:26 -0700

    updated css

--- portfolio/nav.js
+++ portfolio/nav.js
@@ -1,49 +1,54 @@
 /*
 ============================================
 ; Title: Assignment 5.3
-; File Name: nav.js
+; File Name: header.js
 ; Author: Professor Krasso 
 ; Date: 3 February 2022
 ; Modified By: Laura Kendl
 ; Description: Demonstrates how to build and end-to-end functional
 ; prototype of my personal portfolio project.
+; Resources:
+; Blackboard: Code provided by Professor Krasso in WEB330 Assignment 5.3 Outline
 ===========================================
 */
 
-class navBar extends HTMLElement {
+class Nav extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
         this.innerHTML = `
+              
         <nav class="navbar">
         <ul class="navbar-nav">
             <li class="nav-item active">
-                <a class="nav-link nav-link-1" href="/">Home</a>
+                <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-2" href="/about">About Laura</a>
+                <a class="nav-link" href="about.html">About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-3" href="/resume">Resume</a>
+                <a class="nav-link" href="resume.html">Resume</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-4" href="/projects">Projects</a>
+                <a class="nav-link" href="projects.html">Projects</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-1" href="/diagrams">Database Diagrams</a>
+                <a class="nav-link" href="diagrams.html">Database Diagrams</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-2" href="/api-unit-tests">API Unit Tests</a>
+                <a class="nav-link" href="api-unit-tests.html">API Unit Tests</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link nav-link-3" href="/bucket-list">Bucket List</a>
+                <a class="nav-link" href="bucket-list.html">Bucket List</a>
             </li>
         </ul>
     </nav>
+                  
         `;
     }
 }
 
-customElements.define("nav-bar-component", NavBar);
+customElements.define("nav-component", Nav);
+
